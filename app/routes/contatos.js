335ec26f6b50dd79347ddf323f5191f982c306dc
vinicius-mv11 <88984568+vinicius-mv11@users.noreply.github.com>
2023-03-03 23:15:19 +0000

    Initial commit

---
+++ app/routes/contatos.js
@@ -0,0 +1,6 @@
+//var controller = require('../controllers/home')();
+module.exports = function(app) {
+  var controller = app.controllers.contatos;
+  app.get('/contatos', controller.listaContatos);
+  app.get('/contatos/:id', controller.obtemContato);
+}
